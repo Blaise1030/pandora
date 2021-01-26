01c4a0c77cf0e6b5bcf43d51647393c40735ba27
btio0002 <btio0002@student.monash.edu>
2021-01-26 11:00:36 +0800

    Completed the redesign

--- pandora/pages/Components/ChatComponent.js
+++ pandora/pages/Components/ChatComponent.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { Card, Container, Grid } from '@material-ui/core';
 import styles from '../../styles/ChatComponent.module.css';
-import cardStyles from '../../styles/ChatCard.module.css';
+import CardTiles from './MessageCardComponent'
+import {VariableSizeList as List} from 'react-window'
 import MessageCardComponent from "./MessageCardComponent";
 import io from 'socket.io-client'
 
@@ -23,110 +24,121 @@ const ChatComponent = () => {
   const ENDPOINT = 'https://pandora-app2021.herokuapp.com/';
 
 
-  // function timeoutFunction() {
-  //   clearTimeout(timeOut);
-  //   setTyping(false)
-  //   socket.emit('noLongerTypingMessage');
-  // }
+  function timeoutFunction() {
+    clearTimeout(timeOut);
+    setTyping(false)
+    socket.emit('noLongerTypingMessage');
+  }
 
-  // function onKeyDownNotEnter() {
-  //   if (!typing) {
-  //     setTyping(true)
-  //     socket.emit('typingMessage');
-  //     timeOutSetter(setTimeout(timeoutFunction, 3000));
-  //   } else {
-  //     clearTimeout(timeOut);
-  //     timeOutSetter(setTimeout(timeoutFunction, 3000));
-  //   }
-  // }
+  function onKeyDownNotEnter() {
+    if (!typing) {
+      setTyping(true)
+      socket.emit('typingMessage');
+      timeOutSetter(setTimeout(timeoutFunction, 3000));
+    } else {
+      clearTimeout(timeOut);
+      timeOutSetter(setTimeout(timeoutFunction, 3000));
+    }
+  }
 
-  // useEffect(() => {
-  //   socket = io(ENDPOINT);
-  //   socket.emit('join')
-  //   return () => {
-  //     socket.emit('disconnect');
-  //     socket.off();
-  //     io.socket.removeAllListeners()
-  //   }
-  // }, [])
+  useEffect(() => {
+    socket = io(ENDPOINT);
+    socket.emit('join')
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+      io.socket.removeAllListeners()
+    }
+  }, [])
 
-  // useEffect(() => {
-  //   socket.on('join-id', (userObject) => {
-  //     setSocketId(userObject.socketId);
-  //     if (userObject && userObject.room) {
-  //       setCurrentRoom(userObject.room);
-  //       socket.emit('agree-join', userObject)
-  //     }
-  //   });
-  //   socket.on('partner-disconnected', () => {
-  //     setCurrentRoom(null)
-  //     setNewHeader("You are disconnected. Searching for John Doe...")
-  //     socket.emit('agree-leave', { socketId: socketId, room: currentRoom })
-  //   });
-  //   socket.on('partner-typing', (payload) => setPartnerTyping(payload))
-  //   socket.on('partner-no-longer-typing', (payload) => setPartnerTyping(payload))
-  // }, [socketId])
+  useEffect(() => {
+    socket.on('join-id', (userObject) => {
+      setSocketId(userObject.socketId);
+      if (userObject && userObject.room) {
+        setCurrentRoom(userObject.room);
+        socket.emit('agree-join', userObject)
+      }
+    });
+    socket.on('partner-disconnected', () => {
+      setCurrentRoom(null)
+      setNewHeader("You are disconnected. Searching for John Doe...")
+      socket.emit('agree-leave', { socketId: socketId, room: currentRoom })
+    });
+    socket.on('partner-typing', (payload) => setPartnerTyping(payload))
+    socket.on('partner-no-longer-typing', (payload) => setPartnerTyping(payload))
+  }, [socketId])
 
-  // useEffect(() => {
-  //   socket.on('message', (message) => {
-  //     setMessageList((oldMsg) => [...oldMsg, message])
-  //     scroll()
-  //   })
-  // }, [])
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessageList((oldMsg) => [...oldMsg, message]);      
+      scroll()
+    })
+  }, [])
 
-  // function scroll() {
-  //   setTimeout(() => {
-  //     var objDiv = document.getElementById("scrollingDiv");
-  //     objDiv.scrollTop = objDiv.scrollHeight;
-  //   }, 0);
-  // }
+  function scroll() {
+    setTimeout(() => {
+      var objDiv = document.getElementById("scrollToBottom");
+      objDiv.scrollTop = objDiv.scrollHeight;
+    }, 0);
+  }
 
-  // const searchNew = () => {
-  //   socket.emit('search-new', { socketId: socketId, room: currentRoom })
-  // }
+  const searchNew = () => {
+    socket.emit('search-new', { socketId: socketId, room: currentRoom })
+  }
 
-  // const sendMessage = (e) => {
-  //   e.preventDefault();
-  //   timeoutFunction()
-  //   if (input && input.length >= 1 && currentRoom) {
-  //     socket.emit('send-message', input)
-  //     setInput("")
-  //     scroll();
-  //   }
-  // }
+  const sendMessage = (e) => {
+    e.preventDefault();
+    timeoutFunction()
+    if (input && input.length >= 1 && currentRoom) {
+      socket.emit('send-message', input)
+      setInput("")      
+      scroll()
+    }
+  }
 
   const onDisconnectButtonClicked = (e) =>{
-    e.preventDefault()
+    e.preventDefault();
     if (tryDisconnect && currentRoom)
       searchNew()
     setTryDisconnect(!tryDisconnect)
   }
 
+  const onInputClick = () => setTryDisconnect(false);
+  const onInputChange = (e) => {
+    onInputClick()
+    setInput(e.target.value)
+  }
+
+  
   return (
     <Grid container style={{ height: "100vh" }}>
-      <Grid item className={styles.chatScroll}>
-        {messageList.map((msg)=>(<ChatCards socketId={socketId} msg={msg} />))}        
-      </Grid>
+      <div id={"scrollToBottom"} className={styles.chatScroll}>
+        {
+        messageList.map((msg,index)=>(
+          <CardTiles 
+            index={index} 
+            key={msg.text} 
+            socketId={socketId} 
+            msg={msg} />))
+        }        
+      </div>
       <Grid item style={{ width: "100%" }}>
         <div className={styles.inputDiv}>
           <div className={tryDisconnect?styles.confirmLeaveButton:styles.leaveButton}
-               onClick={(e) => onDisconnectButtonClicked(e)}>
+               onClick={onDisconnectButtonClicked}>
             <span style={{ color: "white" }}>{tryDisconnect ? "sure ?" : "leave"}</span>
           </div>
           <form
             style={{ width: "100%" }}
-            onSubmit={(e) => { e.preventDefault(); sendMessage(e)}}>
+            onSubmit={sendMessage}>
             <input
               type="text"
               value={input}
               className={styles.inputStyle}
               placeholder={"Type something ... "}              
-              onClick={(e) => setTryDisconnect(false)}
-              //onKeyDown={(e) => onKeyDownNotEnter()}
-              onChange={(e) => {
-                setTryDisconnect(false);
-                setInput(e.target.value);
-              }}              
+              onClick={onInputClick}
+              onKeyDown={onKeyDownNotEnter}
+              onChange={onInputChange}              
             />
           </form>
         </div>
@@ -138,24 +150,3 @@ const ChatComponent = () => {
 export default ChatComponent;
 
 
-const ChatCards = ({ socketId, msg }) => {
-  if (msg && msg.socketId === 'admin') {
-    return (
-      <div style={{ width: "100%" }}>
-        <div className={cardStyles.adminStyle}> {msg.text} </div>
-      </div>
-    );
-  }
-  else if (msg && socketId && msg.socketId === socketId)
-    return (
-      <div style={{ width: "100%" }}>
-        <div className={cardStyles.userStyle}> {msg.text}  </div>
-      </div>
-    );
-  else
-    return (
-      <div style={{ width: "100%" }}>
-        <div className={cardStyles.otherStyle}> {msg ? msg.text : ""}  </div>
-      </div>
-    );
-}
--- pandora/pages/Components/MessageCardComponent.js
+++ pandora/pages/Components/MessageCardComponent.js
@@ -1,46 +1,26 @@
-function MessageCardComponent(props) {
-  return (
-    <div style={
-      (props.msg && props.socketId === props.msg.socketId) ? {
-        textAlign: "start",
-        wordWrap: "break-word",
-        maxWidth: "40%",
-        marginRight: "26px",
-        marginLeft: "auto",
-        color: "white",
-        width: "fit-content",
-        padding: "12px",
-        marginTop: "10px",
-        marginBottom: "10px",
-        textShadow: '0px 0px 40px black',
-        borderRadius: "13px 2px 10px 13px",
-        boxShadow: "0 8px 10px 0 rgba( 31, 38, 135, 0.1 )",
-        background: 'rgba(63, 195, 128, 0.3)',
-        backdropFilter: 'blur( 20.0px )',
-        WebkitBackdropFilter: 'blur( 20.0px )',
-      } :
-        {
-          textAlign: "start",
-          wordWrap: "break-word",
-          maxWidth: "40%",
-          marginRight: "auto",
-          marginLeft: "26px",
-          color: "black",
-          background: "blue",
-          width: "fit-content",
-          padding: "12px",
-          marginTop: "10px",
-          marginBottom: "10px",
-          textShadow: '0px 0px 40px black',
-          background: 'rgba(255, 255, 255, 0.30)',
-          borderRadius: "2px 13px 13px 20px",
-          backdropFilter: "blur( 4.5px )",
-          WebkitBackdropFilter: "blur( 4.5px )",
-          boxShadow: "0 8px 10px 0 rgba( 31, 38, 135, 0.1 )",
-        }
-    }>
-      {props.msg && props.msg.text ? props.msg.text : ""}
-    </div>)
+import cardStyles from '../../styles/ChatCard.module.css';
+import React from 'react';
+const ChatCards = ({ socketId, msg }) => {
+  if (msg && msg.socketId === 'admin') {
+    return (
+      <div style={{ width: "100%" }}>
+        <div className={cardStyles.adminStyle}> {msg.text} </div>
+      </div>
+    );
+  }
+  else if (msg && socketId && msg.socketId === socketId)
+    return (
+      <div style={{ width: "100%" }}>
+        <div className={cardStyles.userStyle}> {msg.text}  </div>
+      </div>
+    );
+  else
+    return (
+      <div style={{ width: "100%" }}>
+        <div className={cardStyles.otherStyle}> {msg ? msg.text : ""}  </div>
+      </div>
+    );
 }
 
-export default MessageCardComponent;
+const MessageCard = React.memo(ChatCards);
+export default MessageCard
